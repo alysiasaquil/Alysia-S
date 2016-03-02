6677cacfd7ab3da99783b97155b43d27e5069a8b
alysiasaquil <alysiasaquil@gmail.com>
2016-03-02 11:25:11 -0800

    I created a searchbar to look for reviews. Still working on the JavaScript part. #1

---
+++ project-one/app.js
@@ -0,0 +1,29 @@
+var foodPlaces = [
+  {
+    name: "Benihana",
+    location: "Irvine, CA",
+    link: "www.yelp.com/"
+  },
+  {
+    name: "Urban Plates",
+    location: "Irvine, CA",
+    link: "www.yelp.com/"
+  },
+  {
+    name: "Ruth's Chris Steak House",
+    location: "Irvine, CA",
+    link: "www.yelp.com/"
+  },
+  {
+    name: "Capital Seafood",
+    location: "Irvine, CA",
+    link: "www.yelp.com"
+  },
+];
+
+var searchDropDownList = document.getElementById('result-list');
+
+function showList(foodPlaces) {
+  var list = document.createElement('div');
+  
+}
