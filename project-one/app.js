48e08acc84e1f70daaf7fd1cdc484cb3030edee5
alysiasaquil <alysiasaquil@gmail.com>
2016-03-03 13:49:57 -0800

    Added an event listener and was able to print my array of objects to the console. Still need to figure out how to display list of items on page. #1

--- project-one/app.js
+++ project-one/app.js
@@ -1,18 +1,37 @@
-var $star_rating = $('.star-rating .fa');
+var myRestaurants = [
+  {
+    name: "Ruth's Chris Steak House",
+    location: "Irvine, CA",
+    rating: 5,
+    description: "The best steakhouse in Irvine, California is Ruth's Chris Steak House. Treat yourself to a USDA Prime steak that arrives sizzling on a 500 degree plate."
+  },
+  {
+    name: "BRIO Tuscan Grille",
+    location: "Irvine, CA",
+    rating: 4,
+    description: "At BRIO, we celebrate life, love & all things Italian!  In our restaurants, the chef-inspired cuisine at BRIO is simply prepared using the finest and freshest ingredients, with an emphasis on high-quality steaks, housemade pasta specialties and flatbreads prepared in an authentic Italian oven."
+  },
+  {
+    name: "Lazy Dog Restaurant & Bar",
+    location: "Irvine, CA",
+    rating: 3,
+    description: "Lazy Dog Restaurant & Bar is a family-run Southern California based restaurant group."
+  },
+  {
+    name: "BJ's Restaurant & Brewhouse",
+    location: "Irvine, CA",
+    rating: 2,
+    description: "Welcome to BJ's Restaurant & Brewhouse! We're the place to go when you're looking for amazing craft beer, delicious pizza selections, and an extensive menu of high quality food--all in a fun, casual, accommodating setting."
+  }
+];
 
-var SetRatingStar = function() {
-  return $star_rating.each(function() {
-    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
-      return $(this).removeClass('fa-star-o').addClass('fa-star');
-    } else {
-      return $(this).removeClass('fa-star').addClass('fa-star-o');
+var search = document.getElementById('search');
+search.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var keyword = document.getElementById('keyword');
+  for (var i = 0; i < myRestaurants.length; i++) {
+    if (myRestaurants[i].name === keyword.value) {
+      console.log(myRestaurants[i])
     }
-  });
-};
-
-$star_rating.on('click', function() {
-  $star_rating.siblings('input.rating-value').val($(this).data('rating'));
-  return SetRatingStar();
+  }
 });
-
-SetRatingStar();
